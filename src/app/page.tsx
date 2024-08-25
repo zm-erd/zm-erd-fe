ea43953b048df6e4fda32da6bac966620a50089b
HANUYOGA\amagr <amagramer@gmail.com>
2024-08-25 17:13:22 +0900

    22-zm-erd-fefuzzi=>초기 셋팅 카피

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,197 @@
-import Image from 'next/image';
+// import { CardBody, CardContainer, CardItem } from '@/components/global/3d-card';
+// import { HeroParallax } from '@/components/global/connect-parallax';
+// import { ContainerScroll } from '@/components/global/container-scroll-animation';
+// import { InfiniteMovingCards } from '@/components/global/infinite-moving-cards';
+// import { LampComponent } from '@/components/global/lamp';
+// import Navbar from '@/components/global/navbar';
+// import { Button } from '@/components/ui/button';
+// import { clients, products } from '@/lib/constant';
+// import { CheckIcon } from 'lucide-react';
+// import Image from 'next/image';
 
 export default function Home() {
+  //WIP: remove fault IMAge for home page
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white lg:static lg:size-auto lg:bg-none dark:from-black dark:via-black">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+    <main className="flex flex-col items-center justify-center">
+      page...
+      {/*<Navbar />*/}
+      {/*<section className="relative flex h-screen w-full flex-col items-center !overflow-visible rounded-md bg-neutral-950 antialiased">*/}
+      {/*  <div className="absolute inset-0 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_35%,#223_100%)]"></div>*/}
+      {/*  <div className="mt-[-100px] flex flex-col md:mt-[-50px]">*/}
+      {/*    <ContainerScroll*/}
+      {/*      titleComponent={*/}
+      {/*        <div className="flex flex-col items-center">*/}
+      {/*          <Button*/}
+      {/*            size={'lg'}*/}
+      {/*            className="group mb-8 flex w-full items-center justify-center gap-4 rounded-full border-t-2 border-[#4D4D4D] bg-[#1F1F1F] p-8 text-2xl transition-all duration-500 hover:bg-white hover:shadow-xl hover:shadow-neutral-500 sm:w-fit md:mb-0"*/}
+      {/*          >*/}
+      {/*            <span className="goup-hover:to-black bg-gradient-to-r from-neutral-500 to-neutral-600 bg-clip-text font-sans text-transparent group-hover:bg-gradient-to-r group-hover:from-black md:text-center">*/}
+      {/*              Start For Free Today*/}
+      {/*            </span>*/}
+      {/*          </Button>*/}
+      {/*          <h1 className="bg-gradient-to-b from-white to-neutral-600 bg-clip-text font-sans text-5xl font-bold text-transparent md:text-8xl">*/}
+      {/*            Automate Your Work With Fuzzie*/}
+      {/*          </h1>*/}
+      {/*        </div>*/}
+      {/*      }*/}
+      {/*    />*/}
+      {/*  </div>*/}
+      {/*</section>*/}
+      {/*<InfiniteMovingCards*/}
+      {/*  className="mt-[-100px] md:mt-[18rem]"*/}
+      {/*  items={clients}*/}
+      {/*  direction="right"*/}
+      {/*  speed="slow"*/}
+      {/*/>*/}
+      {/*<section>*/}
+      {/*  <HeroParallax products={products}></HeroParallax>*/}
+      {/*</section>*/}
+      {/*<section className="mt-[-500px]">*/}
+      {/*  <LampComponent />*/}
+      {/*  <div className="-mt-72 flex flex-col flex-wrap items-center justify-center gap-8 md:flex-row">*/}
+      {/*    <CardContainer className="inter-var">*/}
+      {/*      <CardBody className="group/card relative h-auto w-full rounded-xl border border-black/[0.1] bg-gray-50 p-6 md:!w-[350px] dark:border-white/[0.2] dark:bg-black dark:hover:shadow-2xl dark:hover:shadow-neutral-500/[0.1]">*/}
+      {/*        <CardItem*/}
+      {/*          translateZ="50"*/}
+      {/*          className="text-xl font-bold text-neutral-600 dark:text-white"*/}
+      {/*        >*/}
+      {/*          Hobby*/}
+      {/*          <h2 className="text-6xl">$0</h2>*/}
+      {/*        </CardItem>*/}
+      {/*        <CardItem*/}
+      {/*          translateZ="60"*/}
+      {/*          className="mt-2 max-w-sm text-sm text-neutral-500 dark:text-neutral-300"*/}
+      {/*        >*/}
+      {/*          Get a glimpse of what our software is capable of. Just a heads*/}
+      {/*          up {"you'll"} never leave us after this!*/}
+      {/*          <ul className="my-4 flex flex-col gap-2">*/}
+      {/*            <li className="flex items-center gap-2">*/}
+      {/*              <CheckIcon />3 Free automations*/}
+      {/*            </li>*/}
+      {/*            <li className="flex items-center gap-2">*/}
+      {/*              <CheckIcon />*/}
+      {/*              100 tasks per month*/}
+      {/*            </li>*/}
+      {/*            <li className="flex items-center gap-2">*/}
+      {/*              <CheckIcon />*/}
+      {/*              Two-step Actions*/}
+      {/*            </li>*/}
+      {/*          </ul>*/}
+      {/*        </CardItem>*/}
+      {/*        <div className="mt-8 flex items-center justify-between">*/}
+      {/*          <CardItem*/}
+      {/*            translateZ={20}*/}
+      {/*            as="button"*/}
+      {/*            className="rounded-xl px-4 py-2 text-xs font-normal dark:text-white"*/}
+      {/*          >*/}
+      {/*            Try now →*/}
+      {/*          </CardItem>*/}
+      {/*          <CardItem*/}
+      {/*            translateZ={20}*/}
+      {/*            as="button"*/}
+      {/*            className="rounded-xl bg-black px-4 py-2 text-xs font-bold text-white dark:bg-white dark:text-black"*/}
+      {/*          >*/}
+      {/*            Get Started Now*/}
+      {/*          </CardItem>*/}
+      {/*        </div>*/}
+      {/*      </CardBody>*/}
+      {/*    </CardContainer>*/}
+      {/*    <CardContainer className="inter-var">*/}
+      {/*      <CardBody className="group/card relative h-auto w-full rounded-xl border border-black/[0.1] bg-gray-50 p-6 md:!w-[350px] dark:border-[#E2CBFF] dark:bg-black dark:hover:shadow-2xl dark:hover:shadow-neutral-500/[0.1]">*/}
+      {/*        <CardItem*/}
+      {/*          translateZ="50"*/}
+      {/*          className="text-xl font-bold text-neutral-600 dark:text-white"*/}
+      {/*        >*/}
+      {/*          Pro Plan*/}
+      {/*          <h2 className="text-6xl">$29</h2>*/}
+      {/*        </CardItem>*/}
+      {/*        <CardItem*/}
+      {/*          translateZ="60"*/}
+      {/*          className="mt-2 max-w-sm text-sm text-neutral-500 dark:text-neutral-300"*/}
+      {/*        >*/}
+      {/*          Get a glimpse of what our software is capable of. Just a heads*/}
+      {/*          up {"you'll"} never leave us after this!*/}
+      {/*          <ul className="my-4 flex flex-col gap-2">*/}
+      {/*            <li className="flex items-center gap-2">*/}
+      {/*              <CheckIcon />3 Free automations*/}
+      {/*            </li>*/}
+      {/*            <li className="flex items-center gap-2">*/}
+      {/*              <CheckIcon />*/}
+      {/*              100 tasks per month*/}
+      {/*            </li>*/}
+      {/*            <li className="flex items-center gap-2">*/}
+      {/*              <CheckIcon />*/}
+      {/*              Two-step Actions*/}
+      {/*            </li>*/}
+      {/*          </ul>*/}
+      {/*        </CardItem>*/}
+      {/*        <div className="mt-8 flex items-center justify-between">*/}
+      {/*          <CardItem*/}
+      {/*            translateZ={20}*/}
+      {/*            as="button"*/}
+      {/*            className="rounded-xl px-4 py-2 text-xs font-normal dark:text-white"*/}
+      {/*          >*/}
+      {/*            Try now →*/}
+      {/*          </CardItem>*/}
+      {/*          <CardItem*/}
+      {/*            translateZ={20}*/}
+      {/*            as="button"*/}
+      {/*            className="rounded-xl bg-black px-4 py-2 text-xs font-bold text-white dark:bg-white dark:text-black"*/}
+      {/*          >*/}
+      {/*            Get Started Now*/}
+      {/*          </CardItem>*/}
+      {/*        </div>*/}
+      {/*      </CardBody>*/}
+      {/*    </CardContainer>*/}
+      {/*    <CardContainer className="inter-var">*/}
+      {/*      <CardBody className="group/card relative h-auto w-full rounded-xl border border-black/[0.1] bg-gray-50 p-6 md:!w-[350px] dark:border-white/[0.2] dark:bg-black dark:hover:shadow-2xl dark:hover:shadow-neutral-500/[0.1]">*/}
+      {/*        <CardItem*/}
+      {/*          translateZ="50"*/}
+      {/*          className="text-xl font-bold text-neutral-600 dark:text-white"*/}
+      {/*        >*/}
+      {/*          Unlimited*/}
+      {/*          <h2 className="text-6xl">$99</h2>*/}
+      {/*        </CardItem>*/}
+      {/*        <CardItem*/}
+      {/*          translateZ="60"*/}
+      {/*          className="mt-2 max-w-sm text-sm text-neutral-500 dark:text-neutral-300"*/}
+      {/*        >*/}
+      {/*          Get a glimpse of what our software is capable of. Just a heads*/}
+      {/*          up {"you'll"} never leave us after this!*/}
+      {/*          <ul className="my-4 flex flex-col gap-2">*/}
+      {/*            <li className="flex items-center gap-2">*/}
+      {/*              <CheckIcon />3 Free automations*/}
+      {/*            </li>*/}
+      {/*            <li className="flex items-center gap-2">*/}
+      {/*              <CheckIcon />*/}
+      {/*              100 tasks per month*/}
+      {/*            </li>*/}
+      {/*            <li className="flex items-center gap-2">*/}
+      {/*              <CheckIcon />*/}
+      {/*              Two-step Actions*/}
+      {/*            </li>*/}
+      {/*          </ul>*/}
+      {/*        </CardItem>*/}
+      {/*        <div className="mt-8 flex items-center justify-between">*/}
+      {/*          <CardItem*/}
+      {/*            translateZ={20}*/}
+      {/*            as="button"*/}
+      {/*            className="rounded-xl px-4 py-2 text-xs font-normal dark:text-white"*/}
+      {/*          >*/}
+      {/*            Try now →*/}
+      {/*          </CardItem>*/}
+      {/*          <CardItem*/}
+      {/*            translateZ={20}*/}
+      {/*            as="button"*/}
+      {/*            className="rounded-xl bg-black px-4 py-2 text-xs font-bold text-white dark:bg-white dark:text-black"*/}
+      {/*          >*/}
+      {/*            Get Started Now*/}
+      {/*          </CardItem>*/}
+      {/*        </div>*/}
+      {/*      </CardBody>*/}
+      {/*    </CardContainer>*/}
+      {/*  </div>*/}
+      {/*</section>*/}
     </main>
   );
 }
